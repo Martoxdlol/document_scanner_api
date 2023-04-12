df1464d8f8a4fb5464fe59aa5b6cfe9a486bae41
Tomás Cichero <martoxdlol@gmail.com>
2023-04-12 13:44:11 -0300

    convert to jpeg first

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import path from 'path'
 import fs from 'fs/promises'
 import { exec } from "child_process";
 import crypto from 'crypto'
+import sharp from 'sharp'
 
 const app = express()
 
@@ -11,16 +12,25 @@ app.use(express.json({ limit: '100mb' }));
 app.post('/scan', async (req, res) => {
     try {
         const [metadata, data] = req.body.data.split(';base64,')
-        const buffer = Buffer.from(data, 'base64')
+        let buffer = Buffer.from(data, 'base64')
+
+        let sharpInstance = sharp(buffer)
+        sharpInstance = sharpInstance.rotate()
+        sharpInstance = sharpInstance.resize({
+            withoutEnlargement: true,
+            fit: 'contain',
+            width: 1600,
+        })
+        buffer = await sharpInstance.toFormat("jpeg").toBuffer()
 
         const sha256Hasher = crypto.createHmac("sha256", '1234');
-        const hash = sha256Hasher.update(req.body.data).digest("hex");
+        const hash = sha256Hasher.update(buffer).digest("hex");
 
-        const filename = 'data/' + hash + '.' + metadata.split('/')[1]
+        const filename = 'data/' + hash + '.jpeg'
 
         try {
             await fs.access(filename);
-            const scanned = await fs.readFile(filename + '.scanned.png', { encoding: 'base64' })
+            const scanned = await fs.readFile(scannedFilename, { encoding: 'base64' })
             console.log(`image ${hash} found`)
             res.send('data:image/png;base64,' + scanned)
             return
@@ -29,11 +39,14 @@ app.post('/scan', async (req, res) => {
         }
 
         await fs.writeFile(filename, buffer)
+
         exec('python3 main.py ' + filename, async (err, stdout) => {
             console.log("Python script output: ", stdout)
             const scanned = await fs.readFile(filename + '.scanned.png', { encoding: 'base64' })
             res.send('data:image/png;base64,' + scanned)
         })
+
+        
     } catch (error) {
         console.error(error)
         res.send(req.body?.data)
